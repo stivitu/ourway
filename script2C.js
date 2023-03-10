15475e110c1e76667774accf2c4f2f0b478543b2
stivitu <67712404+stivitu@users.noreply.github.com>
2023-03-10 16:26:37 +0100

    Add files via upload
    initial commit

---
+++ script2C.js
@@ -0,0 +1,51 @@
+const array2 = [
+ "Ratnik ne treba osobnu povijest. Jednoga dana, shvativši da je više ne treba, ostaviti će je iza sebe.",
+ "Život je neizmjeriv. Takvi smo i mi, a i svako živo biće koje živi na Zemlji.",
+ "Kada stvari postanu nejasne, ratnik razmišlja o svojoj smrti. Jedino ideja o smrti očvršćuje naš duh. A smrt će mu reči: 'Sve je u redu, nisam te još dotaknula'. ",
+ "Ratnik radi stvari zbog višeg cilja, a ne zbog osobne koristi.",
+ "Akcije većine ljudi su usmjerene na osobni dobitak. Ratnikova djelovanja su u korist Duha.",
+ "Duhovno se nudi i otkriva svakome istim intenzitetom i postojanošću. Jedino ratnik ima privilegiju da to i spozna.",
+ "Ratnik mora uzgajati ideju da ima sve što mu je potrebno za delikatno putovanje kao što je život.",
+ "Ono što ratniku treba je saznanje da živi. I to je sve što mu je potrebno.",
+ "Ratnik ne dobiva svoje bitke razbijajući glavom zid. On ga zaobilazi ili preskače preko njega.",
+ "Cilj šamanizma je promijeniti naše shvaćanje o svijetu. To se jedino može postići zaustavljanjem unutarnjeg monologa.",
+ "Put šamana je put povratka. On se vraća iz nepoznatog sa trofejima za Duh. Znanje je jedan od njih.",
+ "Svaki puta kada zaustavimo unutarnji monolog, svijet se urušava i otkriva nam se čudesnost, koja kao da je bila snažno čuvana našim vlastitim mislima.",
+ "Znanje je najčudesnija stvar, pogotovo za ratnika. Znanje je za njega nešto što dolazi naglo, preplavljuje ga i potom odlazi.",
+ "Ratnik razmišlja o sebi kao da je već mrtav. Stoga nema što izgubiti. Najgore mu se je već dogodilo, stoga je smiren. ",
+ "Kada ratnik nauči zaustaviti unutarnji dijalog sve postaje moguće; najkompliciranije zamisli postaju ostvarive.",
+ "Jedina opcija koju ratnik ima jest da radi konstantno i bezrezervno. U određenom momentu, upoznavajući put ratnika, moći će djelovati potpuno usklađeno. Samo stare navike mogu mu još stati na put...",
+ "Kada ratnik naumi uspjeti u nečemu, uspjeh mora doći nježno. Uz puno uloženog truda, ali bez strasti ili opsesije.",
+ "Postoje mnoge stvari koje ratnik može napraviti sada, a nije ih mogao napraviti u godinama prije. Te stvari nisu se promijenile, ali se promijenio njegov stav o sebi.",
+ "Ratnik ne žali za svojim postupcima. Okarakterizirati neko svoje djelovanje kao dobro ili loše, bilo bi pridavanje neopravdanog značenja sebi i svojem djelovanju.",
+ "Ratnik djeluje kao da zna apsolutno sve o tome što radi, čak i ako o situaciji ne zna ništa.",
+ "Ratnik mora činiti tako, da svaka njegova akcija bude smislena. Inače neće ostati na ovome svijetu dovoljno dugo, kako bi se divio njegovim čudesima.",
+ "Lovac-ratnik djeluje u skladu sa svijetom, ali je u isto vrijeme nevidljiv za svijet. On djeluje tiho, ostane koliko je potrebno i zatim odlazi jedva ostavljajući traga.",
+ "Ratnik će biti i dostupan i nedostupan, prema potrebi. Besmisleno je biti dostupan čitavo vrijeme, isto kao što je besmisleno skrivati se, kada svi znaju da se skrivaš.",
+ "Kada se ratnik odluči za akciju, mora je provesti do kraja i preuzeti odgovornost za svoje postupke. Prvo mora znati zašto to radi i odraditi naum sa sigurnošću, bez sumnji i žaljenja.",
+ "Čovjek koji misli o sebi kao da je najvažnija osoba na svijetu, ne može cijeniti svijet oko sebe. Onaj koji vidi samo sebe, odvojen je od svega ostaloga.",
+ "Ratnik uvijek, za razliku od prosječnog čovjeka, preuzima odgovornost za svoje postupke, čak i za one najtrivijalnije.",
+ "Prosječan čovjek previše vremena troši na to da bi se sviđao sebi ili drugima. Ratniku se sve sviđa, i odabire što će mu se i kako sviđati.",
+ "Nema praznine u životu ratnika. Sve je puno do ruba i sve je isto.",
+ "Ratnik odabire put koji ima srca, i prolazi ga u cijelosti. Osjeća i raduje se jer zna da će život ionako brzo završiti i da ništa nije bitnije od nečeg drugog. ",
+ "Ratnik živi djelujući. Ne razmišlja o svojim budućim djelovanjima, niti razmišlja o završenom djelu.",
+ "Put ratnika je najbolji put. Ratnik možda brine i razmišlja prije djelovanja. Ali jednom kad je odluka donesena, on nastavlja svoj put ne razmišljajući o tome, jer zna da će morati donijeti još mnoge odluke u svojem životu.",
+ "Ratnik zna da je samo čovjek ograničen vremenom. I zna da će neke stvari koje ga zanimaju ostati neistražene. Ali za njega to nije problem, to je samo šteta.",
+ "Osjećaj samovažnosti čini čovjeka nespretnim i taštim. Ratnik mora biti prozračan i fluidan.",
+ "Čovjek ima četiri neprijatelja. Strah, jasnoću koja proizlazi iz znanja, moć koja proizlazi iz dosljednosti na putu ratnika i starost. Prva tri se mogu kontrolirati. Ne i starost. Njezini efekti se mogu ublažiti, no ne i izbjeći.",
+ "Svi su putevi isti. Nijedan ne vodi nigdje. Jedan vodi u šumu, drugi kroz šumu. Zato ratnik bira put koji ima srca, i prolazi ga u cijelosti. Takav put će prijeći s uživanjem i radošću, dok bi mu onaj drugi put samo oduzimao energiju i životnu radost. Zato si ratnik uvijek postavlja pitanje: 'Da li ovaj put ima srca?' ",
+ "Ratnik ne pridaje preveliko značenje ljudskim djelovanjima, stoga se on ne može ljutiti na ljude. Ljudska djelovanja nikada ne mogu zasjeniti jednu činjenicu. Naš neizbježni susret s vječnošću.",
+ "Ništa vrijedno na ovom svijetu se ne dobija na poklon. Za to je potrebno uložiti trud i vrijeme.",
+ "Moć leži u vrsti znanja koju posjedujemo. Kakva je korist od znanja koje ne možemo upotrijebiti?",
+ "Nijedno ljudsko djelovanje nije vrijednije od Svijeta. Zato je Svijet za ratnika neiscrpna zagonetka.",
+ "Ratniku mjesto ne igra nikakvu ulogu. Dok god može doći do piva i grickalica :)"
+]
+
+ //array 40 članova
+
+function myFunction2() {
+let randomNumber2 = Math.floor(Math.random() * 40);
+let randomTip2 = array2[randomNumber2];
+document.getElementById("hrvatski").innerText= randomTip2;
+}
+myFunction2();
---
+++ scriptC.js
@@ -0,0 +1,55 @@
+
+
+
+   const array1 = [
+    "A warrior doesn't need personal history. One day, he finds it is no longer necessary for him, and he drops it.",
+    "The world is unfathomable. And so are we, and so is every being that exists in this world.",
+    "A warrior thinks of his death when things become unclear. And death says: 'It is all right, I did not touch you yet.' The idea of death is the only thing that tempers our spirit.",
+    "Warriors have an ulterior purpose for their acts, which has nothing to do with personal gain. The average man acts only if there is the chance for profit. Warriors act not for profit, but for the spirit.",
+    "The spirit manifests itself to a warrior at every turn. However, this is not the entire truth. The entire truth is that the spirit reveals itself to everyone with the same intensity and consistency, but only warriors are consistently attuned to such revelations.",
+    "A warrior must cultivate the feeling that he has everything needed for the extravagant journey that is his life. What counts for a warrior is being alive. Life in itself is sufficient, self-explanatory and complete.",
+    "Warriors do not win victories by beating their heads against walls, but by overtaking the walls. Warriors jump over walls, they don't demolish them.",
+    "To change our idea of the world is the crux of shamanism. And stopping the internal dialogue is the only way to accomplish it.",
+    "Shamanism is a journey of return. A warrior returns from unknown with victory for the spirit. And returning, he brings trophies. Understanding is one of them.",
+    "Whenever the internal dialogue stops, the world collapses, and extraordinary facets of ourselves surface, as though they had been kept heavily guarded by our words.",
+    "Knowledge is a most peculiar affair, especially for a warrior. Knowledge for a warrior is something that comes at once, engulfs him, and passes on.",
+    "A warrior considers himself already dead, so there is nothing for him to lose. The worst has already happened to him, therefore he's clear and calm; judging him by his acts or by his words, one would never suspect that he has witnessed everything.",
+    "When a warrior learns to stop the internal dialogue, everything becomes possible; the most far-fetched schemes become attainable.",
+    "If a warrior is to succeed in anything, the success must come gently, with a great deal of effort but with no stress or obsession.",
+    "The only possible course that a warrior has is to act consistently and without reservations. At a certain moment, he knows enough of the warriors' way to act accordingly, but his old habits and routines may stand in his way.",
+    "There are lots of things a warrior can do at a certain time which he couldn't do years before. Those things themselves did not change; what changed was his idea of himself.",
+    "A warrior doesn't know remorse for anything he has done, because to isolate one's acts as being mean, or ugly, or evil is to place an unwarranted importance on the self.",
+    "A warrior acts as if he knows what he is doing, when in effect he knows nothing.",
+    "A warrior must learn to make every act count, since he is going to be here in this world for only a short while, in fact, too short for witnessing all the marvels of it.",
+    "A warrior-hunter deals intimately with his world, and yet he is inaccessible to that same world. He taps it lightly, stays for as long as he needs to, and then swiftly moves away, leaving hardly a mark.",
+    "A warrior must learn to be available and unavailable at the precise turn of the read. It is useless for a warrior to be unwittingly available at all times, as it is useless for him to hide when everybody knows that he is hiding.",
+    "Whenever a warrior decides to do something he must go all the way, but he must take responsibility for what he does. No matter what he does, he must know first why he is doing it, and then he must proceed with his actions without having doubts or remorse about them.",
+    "As long as man feels that he is the most important thing in the world, he cannot really appreciate the world around him. He is like a horse with blinders; all he sees is himself, apart from everything else.",
+    "A warrior takes responsibility for his acts, for the most trivial of his acts. An average man acts out his thoughts, and never takes responsibility for what he does.",
+    "An average man is too concerned with liking people or with being liked himself. A warrior likes, that's all. He likes whatever or whomever he wants for the hell of it.",
+    "There's no emptiness in the life of a warrior. Everything is filled to the brim. Everything is filled to the brim, and everything is equal.",
+    "A warrior chooses a path with heart, any path with heart, and follows it; and then he rejoices and laughs. He knows because he 'sees' that his life will be over altogether too soon. He 'sees' that nothing is more important that anything else.",
+    "A warrior lives by acting, not by thinking about acting, nor by thinking about what he will think when he has finished acting.",
+    "The most effective way to live is as a warrior. A warrior may worry and think before making any decision, but once he makes it, he goes on his way, free from worries or thoughts; there will be a million other decisions still awaiting him. ",
+    "A warrior knows that he is only a man. His only regret is that his life is so short that he can't grab onto all the things that he would like to. But for him, this is not an issue; it's only a pity. ",
+    "Feeling important makes one heavy, clumsy and vain. To be a warrior one needs to be light and fluid.",
+    "A man has four natural enemies: fear, clarity, power, and old age. Fear, clarity and power can be overcome, but not old age. Its effect can be postponed, but it can never be overcome.",
+    "All paths are the same: they lead nowhere. However, a path without a heart is never enjoyable. On the other hand, a path with heart is easy - it does not make a warrior work at liking it; it makes for a joyful journey;  as long as a man follows it, he is one with it.",
+    "There is a question that a warrior has to ask, mandatorily: Does this path have a heart?",
+    "To be angry at people means that one considers their acts to be important. It is imperative to cease to feel that way. The acts of men cannot be important enough to offset our only viable alternative: our unchangeable encounter with infinity.",
+    "Nothing in this world is a gift. Whatever has to be learned must be learned the hard way.",
+    "Power rests on the kind of knowledge that one holds. What is the sense of knowing things that are useless? They will not prepare us for our unavoidable encounter with the unknown.",
+    "The things that people do cannot under any conditions be more important than the world. And thus a warrior treats the world as an endless mystery and what people do as an endless folly.",
+    "Dwelling upon the self too much produces a terrible fatigue. A man in that position is deaf and blind to everything else. The fatigue makes him cease to see the marvels all around him.",
+    "For a warrior place has no importance. As long he has access to snacks and beer :)"
+     ]
+    //array 40 članova
+function myFunction1() {
+ let randomNumber1 = Math.floor(Math.random() * 40);
+ let randomTip1 = array1[randomNumber1];
+ document.getElementById("engleski").innerText= randomTip1;
+ }
+myFunction1();
+
+
+   
